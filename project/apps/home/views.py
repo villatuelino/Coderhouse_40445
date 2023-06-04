d450ef0e5d2db421746b685aa79fde1625ddab59
Esteban Acevedo <esteban.horacio.acevedo@gmail.com>
2023-06-04 20:47:01 -0300

    Login mejorado

--- project/apps/home/views.py
+++ project/apps/home/views.py
@@ -1,9 +1,8 @@
 from django.contrib.admin.views.decorators import staff_member_required
-from django.contrib.auth import authenticate, login
-from django.contrib.auth.decorators import login_required
-from django.contrib.auth.forms import AuthenticationForm
 from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.urls import reverse, reverse_lazy
+from django.views.generic.edit import CreateView
 from venta import models
 
 from . import forms
@@ -18,29 +17,13 @@ def index(request: HttpRequest) -> HttpResponse:
     return render(request, "home/index.html")
 
 
-def login_request(request: HttpRequest) -> HttpResponse:
-    if request.method == "POST":
-        form = forms.CustomAuthenticationForm(request, data=request.POST)
-        if form.is_valid():
-            usuario = form.cleaned_data.get("username")
-            contraseña = form.cleaned_data.get("password")
-            user = authenticate(username=usuario, password=contraseña)
-            if user is not None:
-                login(request, user)
-                return render(request, "home/index.html", {"mensaje": f"Se ha logueado correctamente."})
-    else:
-        form = AuthenticationForm()
-    return render(request, "home/login.html", {"form": form})
-
-
 @staff_member_required
 def register(request: HttpRequest) -> HttpResponse:
     if request.method == "POST":
         form = forms.CustomUserCreationForm(request.POST)
         if form.is_valid():
-            username = form.cleaned_data["username"]
             form.save()
-            return render(request, "home/index.html", {"mensaje": "Vendedor creado 👌"})
+            return render(request, "home/index.html", {"messages": "Vendedor creado 👌"})
     else:
         form = forms.CustomUserCreationForm()
     return render(request, "home/register.html", {"form": form})
